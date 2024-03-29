8633a621988533363ad3f4fea07e0ec5f1cea53a
Romy <mail@romy.dev>
2024-03-29 13:12:39 +0100

    health stuffies

--- Killing Haven/Assets/Scripts/Interactable.cs
+++ Killing Haven/Assets/Scripts/Interactable.cs
@@ -1,15 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public abstract class Interactable : MonoBehaviour
 {
+    //Add or remove an InteractionEvent component to this gameobject.
+    public bool useEvents;
     //message displayes to player when looking at an interactable
+    [SerializeField]
     public string promptMessage;
+
+    public virtual string OnLook()
+    {
+        return promptMessage;
+    }
 
     // Start is called before the first frame update
     public void BaseInteract()
     {
+        if (useEvents)
+            GetComponent<InteractionEvent>().OnInteract.Invoke();
         Interact();
     }
 
