93b62743af0a6e89f5c21fab9932ecf26d6d7eda
XMintGreenX <36018@ma-web.nl>
2024-04-07 17:47:07 +0200

    Video 5 attack and combat

--- Killing Haven/Assets/Scripts/Enemy/Enemy.cs
+++ Killing Haven/Assets/Scripts/Enemy/Enemy.cs
@@ -6,23 +6,63 @@ public class Enemy : MonoBehaviour
 {
     private StateMachine stateMachine;
     private NavMeshAgent agent;
+    private GameObject player;
 
     public NavMeshAgent Agent { get => agent; }
-    [SerializeField]
-    //Just for debugging purposes.
-    private string currentState;
+    public GameObject Player { get => player; }
     public Path path;
+    [Header("Sight Values")]
+    public float sightDistance = 20f;
+    public float fieldOfView = 85f;
+    public float eyeHeight;
+    [Header("Weapon Values")]
+    public Transform gunBarrel;
+    [Range(0.1f,10)]
+    public float fireRate;
+    //Just for debugging purposes.
+    [SerializeField]
+    private string currentState;
     // Start is called before the first frame update
     void Start()
     {
         stateMachine = GetComponent<StateMachine>();
         agent = GetComponent<NavMeshAgent>();
         stateMachine.Initialise();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        canSeePlayer();
+        currentState = stateMachine.activeState.ToString();
+    }
+    public bool canSeePlayer()
+    {
+        if(player != null)
+        {
+
+            //is the player close enough to be seen?
+            if(Vector3.Distance(transform.position,player.transform.position) < sightDistance)
+            {
+                Vector3 targetDirection = player.transform.position - transform.position - (Vector3.up * eyeHeight);
+                float angleToPlayer = Vector3.Angle(targetDirection, transform.forward);
+
+                if(angleToPlayer >= -fieldOfView && angleToPlayer <= fieldOfView)
+                {
+                    Ray ray = new Ray(transform.position + (Vector3.up * eyeHeight), targetDirection);
+                    RaycastHit hitinfo = new RaycastHit();
+                    if(Physics.Raycast(ray,out hitinfo,sightDistance))
+                    {
+                        if (hitinfo.transform.gameObject == player)
+                        {
+                            Debug.DrawRay(ray.origin, ray.direction * sightDistance);
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
